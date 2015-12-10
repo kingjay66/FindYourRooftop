851ef4ae6f9f342d42f6a1800064840fc7e0440d
Tim Lai <timothy.lai@gmail.com>
2015-12-09 23:24:16 -0800

    fix/heroku env

--- server/yelp.js
+++ server/yelp.js
@@ -1,15 +1,25 @@
 var Yelp = require('yelp');
-
-var yelp = new Yelp(process.env.YELP);
+// var config = require('../config.js');
+// console.log(process.env.YELP);
+// var config_yelp = process.env.YELP || config.;
+// var yelp = new Yelp(process.env.YELP);
 // var yelp = new Yelp({
 //   consumer_key: 'QfssABWRjYQulw4wvMx7kQ',
 //   consumer_secret: '82PKYE32zGwYsqKd5UGiedWE7L0',
 //   token: '1ei2Ny4uKw-TaE8ZOfkGwwUyjyIrGE0h',
 //   token_secret: 'tYUurnVyxQAC5p-dwDKDIlPdfA8',
 // });
+var yelp = new Yelp({
+  consumer_key: process.env.yelp_consumer_key,
+  consumer_secret: process.env.yelp_consumer_secret,
+  token: process.env.yelp_token,
+  token_secret: process.env.yelp_token_secret, 
+});
+// var yelp = new Yelp(config_yelp);
 
 // See http://www.yelp.com/developers/documentation/v2/search_api
 exports.callYelpApi = function(req, res, next){
+  console.log(yelp);
   var barData =[];
   console.log('yelp.js -- req.body: ' + JSON.stringify(req.body, null, 2));
   yelp.search(req.body)
